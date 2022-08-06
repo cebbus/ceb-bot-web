6c96e7a09ad8a00eb4c7871f08f425bbb4271fa5
Sedat Pehlivanlı <cebbus@gmail.com>
2022-08-06 20:43:06 +0300

    init cebbot web

---
+++ src/main/java/com/cebbus/bot/web/CebBotWebApplication.java
@@ -0,0 +1,13 @@
+package com.cebbus.bot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CebBotWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CebBotWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cebbus/bot/web/CebBotWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cebbus.bot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CebBotWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
